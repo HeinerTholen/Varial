9ed1d29fd90544d84e1b7c28ce3d97048605ba47
Heiner Tholen <github@heinertholen.com>
2014-03-10 11:51:53 +0100

    init file without batch mode

--- varial/__init__.py
+++ varial/__init__.py
@@ -1,2 +1,12 @@
 import ROOT
-ROOT.gROOT.SetBatch()
+#ROOT.gROOT.SetBatch()
+
+import settings
+for member in dir(settings):
+    if member[:4] == "DIR_":
+        setattr(settings, member, "./")
+
+import generators as gen
+import operations as op
+import wrappers as wrp
+import diskio as dio
