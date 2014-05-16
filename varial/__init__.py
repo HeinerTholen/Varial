c29df11211e2a28366cf24a8c386b6dddbdb7674
Heiner Tholen <heiner.tholen@cern.ch>
2014-05-16 19:32:31 +0200

    making standard import somewhat lighter

--- varial/__init__.py
+++ varial/__init__.py
@@ -1,11 +1,7 @@
 import diskio
 import generators
-import gridutil
 import operations
-import postprocessing
-import postproctools
 import rendering
-import sample
 import settings
 import wrappers
 
