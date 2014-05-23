35fd1098faa299c7dcc38776e9ab3b3995b515fa
Heiner Tholen <heiner.tholen@cern.ch>
2014-05-23 16:18:07 +0200

    fwlite interface fully implemented / untested

--- varial/fwliteproxy.py
+++ varial/fwliteproxy.py
@@ -1,6 +1,11 @@
+import os
+import subprocess
+import time
+
 import analysis
-import settings
 import diskio
+import monitor
+import settings
 import toolinterface
 
 
@@ -10,14 +15,51 @@ class FwliteProxy(toolinterface.Tool):
                  py_exe=settings.fwlite_executable):
         super(FwliteProxy, self).__init__(name)
         self.py_exe = py_exe
+        self._proxy = None
+
+    def wanna_reuse(self, all_reused_before_me):
+
+        if not os.path.exists(os.path.join(
+                self.result_dir, 'fwlite_proxy.info')):
+            return False
+        proxy = diskio.read('fwlite_proxy')
+        if not hasattr(proxy, 'results'):
+            return False
+        for name, smp in analysis.all_samples.iteritems():
+            if not (
+                name in proxy.samples
+                and proxy.samples[name] == smp.input_files
+            ):
+                return False
+        self._proxy = proxy
+        return True
 
     def reuse(self):
-        pass  # check for samples
+        self._finalize()
 
     def run(self):
-        pass
+        self._proxy = analysis.fileservice('fwlite_proxy', False)
+        self._proxy.event_files = dict(
+            (s.name, s.input_files)
+            for s in analysis.all_samples.itervalues()
+        )
+        diskio.write(self._proxy)
+        proc = subprocess.Popen(
+            ['python', self.py_exe],
+            stdout=monitor._info.outstream,
+            stderr=subprocess.STDOUT
+        )
+        while None == proc.returncode:
+            time.sleep(0.2)
+            proc.poll()
+        self._finalize()
 
-        # remove old proxy ...
-
-        proxy = diskio.read('fwlite_proxy')
-        # proxy.event_files
+    def _finalize(self):
+        for res in self._proxy.results:
+            samplename = res.split('!')[0]
+            analysis.fs_aliases += list(
+                alias for alias in diskio.generate_fs_aliases(
+                    os.path.join(self.result_dir, '%s.root' % res),
+                    samplename
+                )
+            )
--- varial/fwliteworker.py
+++ varial/fwliteworker.py
@@ -10,41 +10,57 @@ import wrappers
 def _add_results(results_iter):
     res_sums = {}
     for results in results_iter:
-        if not res_sums:
-            res_sums = dict((r.name, r) for r in results)
-        else:
-            for res in results:
-                res_sum = res_sums[res.name]
-                for k, v in res.__dict__.iteritems():
+        for new_res in results:
+            if new_res.name in res_sums:
+                res_sum = res_sums[new_res.name]
+                for k, v in new_res.__dict__.iteritems():
                     if isinstance(v, ROOT.TH1):
                         getattr(res_sum, k).Add(v)
+            else:
+                res_sums[new_res.name] = new_res
     return res_sums
 
 
 def _run_workers(args):
-    event_handle, workers = args
+    event_handle_wrp, workers = args
     for w in workers:
-        w.node_setup(event_handle)
-    for event in event_handle:
+        w.node_setup(event_handle_wrp)
+    for event in event_handle_wrp.event_handle:
         for w in workers:
             w.node_process_event(event)
     for w in workers:
         w.node_finalize()
+        if hasattr(event_handle_wrp, 'sample'):
+            w.result.sample = event_handle_wrp.sample
+            w.result.name = '%s!%s' % (event_handle_wrp.sample, w.result.name)
     results = list(w.result for w in workers)
     return results
 
 
 def work(workers, event_handles=None, use_mp=True):
-
     proxy = None
     if not event_handles:
         if not os.path.exists('fwlite_proxy.info'):
             raise RuntimeError('You must either provide the event_handles '
                                'argument or fwlite_proxy.info in my cwd!')
         proxy = diskio.read('fwlite_proxy')
-        from DataFormats.FWLite import Events
-        event_handles = (Events(f) for f in proxy.event_files)
         use_mp = proxy.__dict__.get('use_mp', use_mp)
+
+        from DataFormats.FWLite import Events
+        def event_handles():
+            for sample, files in proxy.event_files.iteritems():
+                for f in files:
+                    h_evt = Events(f)
+                    yield wrappers.Wrapper(
+                        event_handle=h_evt,
+                        sample=sample,
+                        filenames=h_evt._filenames,
+                    )
+    else:
+        event_handles = (wrappers.Wrapper(
+            event_handle=h_evt,
+            filenames=h_evt._filenames
+        ) for h_evt in event_handles)
 
     if use_mp:
         imap_func = multiprocessing.Pool().imap_unordered
@@ -62,7 +78,7 @@ def work(workers, event_handles=None, use_mp=True):
     if proxy:
         for r in res.values():
             diskio.write(r)
-        proxy.results = list(w.name for w in res)
+        proxy.results = res.keys()
         diskio.write(proxy)
     return res
 
