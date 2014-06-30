682aa5b97cad49fe7f39620278257b6ea9f14a77
Heiner Tholen <github@heinertholen.com>
2014-06-30 10:32:30 +0200

    dbio init file

---
+++ varial/dbio.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+import analysis
+
+
+def write(wrp):
+    pass
+
+
+def read(name):
+    pass
+
+
+def get(name):
+    pass
+
+
+def generate_aliases(path=None):
+    pass
+
+
+def load_histogram(alias):
+    pass
