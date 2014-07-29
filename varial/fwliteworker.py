f63ce833a684258456dfe462a4ae85e4ad68ee9d
Heiner Tholen <heiner.tholen@cern.ch>
2014-07-29 17:12:40 +0200

    fix: worker without result is not deleted any longer

--- varial/fwliteworker.py
+++ varial/fwliteworker.py
@@ -74,6 +74,7 @@ def _run_workers(event_handle_wrp):
             do_the_eventloop()
 
     # finalize workers
+    workers_with_result = []
     for w in workers[:]:
         try:
             w.node_finalize(event_handle_wrp)
@@ -84,16 +85,15 @@ def _run_workers(event_handle_wrp):
                 print event_handle_wrp
                 print '\n'
             raise e
-        if w.result.is_empty():
-            workers.remove(w)
-            continue
-        if hasattr(event_handle_wrp, 'sample'):
-            w.result.sample = event_handle_wrp.sample
-            w.result.id = '%s!%s' % (event_handle_wrp.sample, w.result.name)
-        else:
-            w.result.id = w.result.name
+        if not w.result.is_empty():
+            workers_with_result.append(w)
+            if hasattr(event_handle_wrp, 'sample'):
+                w.result.sample = event_handle_wrp.sample
+                w.result.id = '%s!%s' % (event_handle_wrp.sample, w.result.name)
+            else:
+                w.result.id = w.result.name
     del event_handle_wrp.event_handle
-    event_handle_wrp.results = list(w.result for w in workers)
+    event_handle_wrp.results = list(w.result for w in workers_with_result)
     return event_handle_wrp
 
 
@@ -141,7 +141,7 @@ def work(workers, event_handles=None):
         os.mkdir('.cache')
 
         from DataFormats.FWLite import Events
-        def event_handles():
+        def event_handle_gen():
             for sample, files in _proxy.event_files.iteritems():
                 for f in files:
                     if sample in _proxy.files_done:
@@ -154,6 +154,7 @@ def work(workers, event_handles=None):
                         filenames=h_evt._filenames,
                         workers=workers,
                     )
+        event_handles = event_handle_gen()
     else:
         event_handles = (wrappers.Wrapper(
             event_handle=h_evt,
@@ -167,5 +168,5 @@ def work(workers, event_handles=None):
     else:
         imap_func = itertools.imap
 
-    results_iter = imap_func(_run_workers, event_handles())
+    results_iter = imap_func(_run_workers, event_handles)
     return _add_results(results_iter)
