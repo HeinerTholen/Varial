899f8714d64843d66045515b91d2d78600e1fb6c
Heiner Tholen <heiner.tholen@cern.ch>
2015-07-14 13:37:43 +0200

    Decorator classes default constructor

--- varial/rendering.py
+++ varial/rendering.py
@@ -382,7 +382,7 @@ class TitleBox(util.Decorator):
     Draws title-box with TPaveText above canvas window.
 
     Instanciate with text argument:
-    ``tb = TitleBox(None, text='My funny title')``.
+    ``tb = TitleBox(text='My funny title')``.
     """
     def do_final_cosmetics(self):
         self.decoratee.do_final_cosmetics()
@@ -400,12 +400,18 @@ class TitleBox(util.Decorator):
 
 
 class TextBox(util.Decorator):
-    """Draw Textboxes individually by renderer name"""
-    def __init__(self, inner=None, dd=True, **kws):
-        super(TextBox, self).__init__(inner, dd, **kws)
-        assert('textbox' in self.dec_par)
+    """
+    Draw Textbox.
+
+    Instanciate with textbox argument:
+    ``tb = TextBox(textbox=ROOT.TLatex(0.5, 0.5, 'My Box'))``.
+    """
 
     def do_final_cosmetics(self):
+        if 'textbox' not in self.dec_par:
+            self.dec_par['textbox'] = ROOT.TLatex(
+                0.5, 0.5,
+                'I need a textbox=ROOT.TLatex(0.5, 0.5, "My Box") parameter')
         self.decoratee.do_final_cosmetics()
         self.dec_par['textbox'].Draw()
 
@@ -420,8 +426,11 @@ class Legend(util.Decorator):
 
     You can set ``draw_option_legend`` on a wrapper. If it evaluates to
     ``False`` (like an empty string), the item will be removed from the legend.
+
+    All default settings in ``settings.defaults_Legend`` can be overwritten by
+    providing an argument with the same name, e.g. ``Legend(x_pos=0.2)``.
     """
-    def __init__(self, inner, dd='True', **kws):
+    def __init__(self, inner=None, dd='True', **kws):
         super(Legend, self).__init__(inner, dd)
         self.dec_par.update(settings.defaults_Legend)
         self.dec_par.update(kws)
@@ -496,7 +505,7 @@ class Legend(util.Decorator):
 
 class BottomPlot(util.Decorator):
     """Base class for all plot business at the bottom of the canvas."""
-    def __init__(self, inner, dd = True, **kws):
+    def __init__(self, inner=None, dd = True, **kws):
         super(BottomPlot, self).__init__(inner, dd, **kws)
         self.dec_par.update(settings.defaults_BottomPlot)
         self.dec_par.update(kws)
@@ -664,3 +673,4 @@ class BottomPlotRatioSplitErr(BottomPlot):
 # TODO use WrapperWrapper
 # TODO make a setting for choosing the default bottom plot
 # TODO BottomPlotSignificance
+# TODO redesign all canvas-making. Abandon Decorators. Use generators.
