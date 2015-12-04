3f1bd793b918a7426f5f4dd51f58698412ad11c1
Heiner Tholen <heiner.tholen@cern.ch>
2015-12-04 16:18:43 +0100

    putting block images into folder for easier deletion

--- varial/extensions/tex.py
+++ varial/extensions/tex.py
@@ -27,9 +27,9 @@ class TexContent(Tool):
     :param dest_dir:    destination directory (default: tool path)
     """
     def __init__(self,
-                 images,
-                 plain_files,
-                 include_str,
+                 images={},
+                 plain_files={},
+                 include_str='%s',
                  dest_dir=None,
                  name=None):
         super(TexContent, self).__init__(name)
@@ -37,15 +37,22 @@ class TexContent(Tool):
         self.tex_files = plain_files
         self.include_str = include_str
         self.dest_dir = dest_dir
+        self.dest_dir_name = None
 
-    def _join(self, basename):
-        return os.path.join(self.dest_dir, basename)
+    def _join(self, *args):
+        return os.path.join(self.dest_dir, *args)
 
     @staticmethod
     def _hashified_filename(path):
         bname, ext = os.path.splitext(os.path.basename(path))
         hash_str = '_' + hex(hash(path))[-7:]
         return bname + hash_str
+
+    def initialize(self):
+        if not self.dest_dir:
+            self.dest_dir = self.cwd
+        p_elems = self.dest_dir.split('/')
+        self.dest_dir_name = p_elems[-1] or p_elems[-2]
 
     def copy_image_files(self):
         for blockname, blockfiles in self.images.iteritems():
@@ -54,6 +61,10 @@ class TexContent(Tool):
             )
 
             # copy image files
+            blockdir = self._join(blockname)
+            if not os.path.exists(blockdir):
+                os.mkdir(blockdir)
+
             for hashified, path in hashified_and_path:
                 p, ext = os.path.splitext(path)
                 if ext == '.eps':
@@ -62,19 +73,20 @@ class TexContent(Tool):
                 elif not ext in ('.pdf', '.png'):
                     raise RuntimeError(
                         'Only .eps, .pdf and .png images are supported.')
-                shutil.copy(p+ext, self._join(hashified+ext))
+                shutil.copy(p+ext, os.path.join(blockdir, hashified+ext))
 
             # make block file
-            with open(self._join(blockname), 'w') as f:
+            folder_name = [-1]
+            with open(self._join(blockname+'.tex'), 'w') as f:
                 for hashified, _ in hashified_and_path:
-                    f.write(self.include_str % hashified + '\n')
+                    cnt = os.path.join(self.dest_dir_name, blockname, hashified)
+                    f.write(self.include_str % cnt + '\n')
 
     def copy_plain_files(self):
         for fname, path, in self.tex_files.iteritems():
             shutil.copy(path, self._join(fname))
 
     def run(self):
-        if not self.dest_dir:
-            self.dest_dir = self.cwd
+        self.initialize()
         self.copy_image_files()
         self.copy_plain_files()
